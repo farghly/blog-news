747be2cb3cd3917189677b90e283de2882b0a320
Farghly <eng_farghlyahmed@yahoo.com>
2021-09-17 18:37:29 +0200

    validate_login

--- includes/validate_login.php
+++ includes/validate_login.php
@@ -5,15 +5,15 @@ include("../includes/config.php"); ?>
 if(isset($_POST['login'])){
   $user_email     = $_POST['user_email'];
   $password       = $_POST['user_password'];
-  $password       =SHA1($password);
+  $password       = SHA1($password);
   $query          = $connect->query("SELECT *FROM authors WHERE author_email='$user_email' And author_password='$password'");  
- // $query->bindValue(':user_email', $_POST['user-emai']);
+//$query->bindValue(':user_email', $_POST['user-emai']);
+
   $getresult  = $query->fetch(PDO::FETCH_ASSOC);
   if(!$getresult){
       echo 'failed';
   }
      
-      
      $author_id     = $getresult['author_id'];
      $author_status = $getresult['author_status'];
      $db_email      = $getresult['author_email'];
@@ -25,7 +25,7 @@ if(isset($_POST['login'])){
         $_SESSION['author_password'] = $db_pass;
         $_SESSION['author_role']     = $db_role;
         $_SESSION['message_success']="Welcome to ".$_SESSION['author_role'];
-        header("Location:../admin");
+        header("Location:../admin/");
     }elseif($user_email === $db_email && $password ===$db_pass && $author_status=='0'){
         $_SESSION['active_account']="You must active account by admin";
         header("location:../login.php");
