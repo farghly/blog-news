a2d3c9daa2316a1ec6823071e5a5be9437f9d25d
Farghly <eng_farghlyahmed@yahoo.com>
2021-09-18 03:46:10 +0200

    update_idexPage for website

--- index.php
+++ index.php
@@ -2,18 +2,10 @@
 define("DS",DIRECTORY_SEPARATOR);
 define("APP_PATH",__DIR__);
 define('INCLUDE_PATH',APP_PATH.DS."includes".DS);
-/*define('CSS_PATH',APP_PATH.DS."assets".DS.'css'.DS);
-define('JS_PATH',APP_PATH.DS."assets".DS.'js'.DS);
-echo CSS_PATH;*/
 
-    /*
-        * includes files as array
-        * define include folder path 
-    */
-
-$includes = array('config.php','functions.php','header.php','body.php');
+$includes = array('config','functions','header','body');
 foreach($includes as $include){
-   require (INCLUDE_PATH.$include);
+   require (INCLUDE_PATH.$include.'.php');
 }
 
 require_once 'includes/footer.php';
