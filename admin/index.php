8a3ecc8d98623d86f42615bf1df9ad6c262a3417
Farghly <eng_farghlyahmed@yahoo.com>
2021-09-18 02:37:42 +0200

    refactor_index_page

--- admin/index.php
+++ admin/index.php
@@ -4,19 +4,11 @@ require('../includes/functions.php');
 define("DS",DIRECTORY_SEPARATOR);
 define("ADMIN_PATH",__DIR__);
 define('INCLUDE_PATH',ADMIN_PATH.DS."includes".DS);
-/*define('CSS_PATH',APP_PATH.DS."assets".DS.'css'.DS);
-define('JS_PATH',APP_PATH.DS."assets".DS.'js'.DS);
-echo CSS_PATH;*/
-
-    /*
-        * includes files as array
-        * define include folder path 
-    */
 
 
-$includes = array('header.php','navbar.php','dashboard.php');
+$includes = array('header','navbar','dashboard');
 foreach($includes as $include){
-   require (INCLUDE_PATH.$include);
+   require (INCLUDE_PATH.$include.'.php');
 }
 
 require_once INCLUDE_PATH.'footer.php';
