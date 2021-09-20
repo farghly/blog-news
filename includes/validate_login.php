ca92aea83f89e4452ed73be8691fa1b30fe229c9
Farghly <eng_farghlyahmed@yahoo.com>
2021-09-20 09:44:54 +0200

    remove dead code from validate_login

--- includes/validate_login.php
+++ includes/validate_login.php
@@ -1,26 +1,24 @@
 <?php
-include("../includes/config.php"); ?>
-<?php session_start(); ?>
-<?php
+include("config.php"); 
+session_start(); 
+
 if(isset($_POST['login'])){
   $user_email     = $_POST['user_email'];
   $password       = $_POST['user_password'];
   $password       = SHA1($password);
   $query          = $connect->query("SELECT *FROM authors WHERE author_email='$user_email' And author_password='$password'");  
 //$query->bindValue(':user_email', $_POST['user-emai']);
-
   $getresult  = $query->fetch(PDO::FETCH_ASSOC);
   if(!$getresult){
       echo 'failed';
-  }
-     
+  }     
      $author_id     = $getresult['author_id'];
      $author_status = $getresult['author_status'];
      $db_email      = $getresult['author_email'];
      $db_pass       = $getresult['author_password'];
      $db_role       = $getresult['author_role'];
     if($user_email === $db_email && $password ===$db_pass && $author_status=='1'&&($db_role === "admin" || $db_role === "author")){
-        $_SESSION['author_id']        = $getresult['author_id'];
+        $_SESSION['author_id']        =  $author_id  ;
         $_SESSION['author_email']    = $db_email;
         $_SESSION['author_password'] = $db_pass;
         $_SESSION['author_role']     = $db_role;
@@ -32,9 +30,5 @@ if(isset($_POST['login'])){
     }else{
        $_SESSION['error_login']="Error in email or password";
         header("location:../login.php"); 
-    }
-            
-     
+    }     
 }
-
-
