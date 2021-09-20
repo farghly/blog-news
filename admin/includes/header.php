3364b40d9487991164d964e2ca8d438582ea5ba1
Farghly <eng_farghlyahmed@yahoo.com>
2021-09-20 09:05:41 +0200

    remove dead code from header

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -1,47 +1,35 @@
-
-<?php ob_start(); ?>
-<?php session_start();
+<?php 
+ob_start(); 
+session_start();
 error_reporting(1);
 header("Cache-Control:cache, store, must-revalidate"); // HTTP 1.1.
-?>
-<?php
+
 if(!isset($_SESSION['author_email']) && !isset($_SESSION['author_password'])) {
 	header("location:../login.php");
 }
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-    
     <title>Admin Panel</title>
     <link rel="shortcut icon" href="img/favicon_3.png">
     <!-- Bootstrap Core CSS -->
-	
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" type="text/css">
    <!--	<link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">-->
     <link rel="stylesheet" href="css/css_tag.css" type="text/css">
-	<!--
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"  >
-	-->
     <!-- Custom CSS -->
     <link href="css/bootstrap-tagsinput.css" type="text/css" rel="stylesheet" />
 	 <link href="css/style.css" rel="stylesheet">
     <link href="css/sb-admin.css" rel="stylesheet">
-	
-
-    <!-- Morris Charts CSS -->
-
-    <!-- Custom Fonts -->
 
     <link rel="stylesheet" href="css/font-awesome.min.css" type="text/css">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css"> 
+	<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">  -->
+    
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
@@ -54,20 +42,15 @@ $urldashborad =$_SERVER['HTTP_HOST']."/admin/";
 $urldashboradindex =$_SERVER['HTTP_HOST']."/admin/index.php";
 if($_SESSION['author_role']=="admin" && ($urldashborad==$link || $urldashboradindex==$link)):
 ?>
+<script src="https://www.gstatic.com/charts/loader.js"></script>
+<?php endif; 
 
-<?php endif; ?>
-<?php 
- $url = $_SERVER['HTTP_HOST']."/admin/view_posts.php?source=add_post";
+$url = $_SERVER['HTTP_HOST']."/admin/view_posts.php?source=add_post";
 if(isset($_GET['p_id'])|| $link == $url) :?> 
     <script src="https://cdn.tiny.cloud/1/ob2z96bqz0k2wmu95zjfl2n4n4pzmmmlsl2zovinrp97sr56/tinymce/5/tinymce.min.js" 
      referrerpolicy="origin"></script>
   <script>tinymce.init({selector:'textarea',plugins: 'table'});</script>
   <link rel="stylesheet" href="css/bootstrap-tagsinput.css" type="text/css">
- 
-
 <?php endif; ?>
-<script src="https://www.gstatic.com/charts/loader.js"></script>
 </head>
-
 <body>
-
