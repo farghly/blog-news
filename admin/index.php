e4f06aab52ee67eece0fe537662305709ddbead1
Farghly <eng_farghlyahmed@yahoo.com>
2021-09-26 05:44:43 +0200

    remove_dead_code_from index admin

--- admin/index.php
+++ admin/index.php
@@ -1,9 +1,5 @@
 <?php
-define("DS",DIRECTORY_SEPARATOR);
-define("ADMIN_PATH",__DIR__);
-define('INCLUDE_PATH',ADMIN_PATH.DS."includes".DS);
-
 $includes = ['config.file','dashboard','footer'];
 foreach($includes as $include){
-   require (INCLUDE_PATH.$include.'.php');
+   require ('includes/'.$include.'.php');
 }
