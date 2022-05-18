d2c4b6a0b04099b3e6427f24872908931bfe16ba
farghly <eng_farghlyahmed@yahoo.com>
2022-05-18 12:31:02 +0200

    updateIndexFile editVariable

--- index.php
+++ index.php
@@ -4,8 +4,8 @@ define("APP_PATH",__DIR__);
 define('INCLUDE_PATH',APP_PATH.DS."includes".DS);
 
 $includes = ['config','functions','header','body','footer'];
-foreach($includes as $include){
-   require_once (INCLUDE_PATH.$include.'.php');
+foreach($includes as $includeFile){
+   require_once (INCLUDE_PATH.$includeFile.'.php');
 }
 
 
